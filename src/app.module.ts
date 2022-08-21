77599078d12873ddbbd90241ea36dafd68e2d97d
Junior Paula <luke.junnior@icloud.com>
2022-08-21 10:56:46 -0300

    feat: add challenges modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ChallengesModule } from './challenges/challenges.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/api-ranking'),
     PlayersModule,
     CategoriesModule,
+    ChallengesModule,
   ],
   controllers: [],
   providers: [],
